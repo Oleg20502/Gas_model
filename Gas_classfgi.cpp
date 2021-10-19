f8ccdc3c9f7ca311d3cd3b55feb740bed36c6253
Oleg20502 <oleg20502@gmail.com>
2021-10-19 22:45:00 +0300

    Removed crucial bug (now energy saves), modified prog and added data files

--- Gas_classfgi.cpp
+++ Gas_classfgi.cpp
@@ -8,29 +8,24 @@
 #include <chrono>
 
 using std::min, std::max, std::sqrt, std::pow, std::abs, std::floor;
+using std::stoi, std::stod;
 
 double vec_mod(double x, double y, double z)
 {
     return sqrt(x*x + y*y + z*z);
 }
 
-double sim_sum(std::vector<std::vector<double>> const &vec, size_t I)
+std::string parse_line(std::ifstream out)
 {
-    double sum = 0;
-    for(int i = 0; i<I; ++i)
-    {
-        sum += vec[i][I];
-    }
-    for(int i = 0; i<vec.size() - I; ++i)
-    {
-        sum += vec[i][vec.size() - I - 1];
-    }
-    return sum;
+    std::string str1, str2;
+    getline(out, str2);
+    getline(out, str1);
+    return str1;
 }
 
+//Alen Computer Simulation of liquids
 double sum(std::vector<double> const &vec)
 {
-
     double sum = 0;
     for(int i = 0; i<vec.size(); ++i)
     {
@@ -52,7 +47,24 @@ double find_min(std::vector<std::vector<double>> const &vec, int N)
     return min;
 }
 
-double force(double r, double eps=1.0, double sigma=1.0)
+double LJP(double r, double eps=1.0, double sigma=1.0)
+{
+    double a = pow(sigma/r, 6);
+    return 4.0*eps*(a - 1)*a;
+}
+
+double LJP_cut(double r, double rmax, double eps=1.0, double sigma=1.0)
+{
+    if (r < rmax){
+        return LJP(r, eps, sigma) - LJP(rmax, eps, sigma);
+    }
+    else{
+        return 0.0;
+    }
+
+}
+
+double LJP_force(double r, double eps=1.0, double sigma=1.0)
     {
         double a = pow(sigma/r, 6);
         return 24.0*eps*(2*a - 1)*a/r;
@@ -69,15 +81,15 @@ struct Point{
 class Space{
 public:
     double x_size, y_size, z_size, r, eps = 1.0, sigma = 1.0;
+    double Energy, P, Px, Py, Pz;
+    double temperature, k = 0.01;
     unsigned int N;
     std::vector<Point> p;
-
     std::vector<std::vector<double>> R;
     std::vector<std::vector<double>> F;
     std::vector<std::vector<double>> Fx;
     std::vector<std::vector<double>> Fy;
     std::vector<std::vector<double>> Fz;
-    //double k = 1.0;
 
     double dx, dy, dz;
 
@@ -88,8 +100,8 @@ public:
 
 
 public:
-    Space(unsigned int n, double x_size, double y_size, double z_size):
-        N{n}, x_size{x_size}, y_size{y_size}, z_size{z_size},
+    Space(unsigned int n, double x_size, double y_size, double z_size, double eps, double sigma):
+        N{n}, x_size{x_size}, y_size{y_size}, z_size{z_size}, eps{eps}, sigma{sigma},
         p{std::vector<Point> (N)},
         F{std::vector<std::vector<double>> (N, std::vector<double> (N))},
         Fx{std::vector<std::vector<double>> (N, std::vector<double> (N))},
@@ -117,23 +129,67 @@ public:
 
         for(int i = 0; i<N-1; ++i){
             for(int j = i+1; j<N; ++j){
-                R[i][j] = get_distance(i, j);
-                R[j][i] = R[i][j];
+                count_forces(i, j);
             }
         }
     }
 
+    void set_grid_points()
+    {
+        double k = pow(static_cast<double> (0.9*N/x_size/y_size/z_size), 1.0/3);
+        int Nx = floor(k*x_size);
+        int Ny = floor(k*y_size);
+        int Nz = floor(k*z_size);
+        std::cout << Nx << '\n';
 
+        for(int i = 0; i<Nx; ++i){
+            for(int j=0; j<Ny; ++j){
+                for(int m=0; m<Nz; ++m){
+                    p[i + j*Nx + m*Nx*Ny].x = i/k+0.1;
+                    p[i + j*Nx + m*Nx*Ny].y = j/k+0.1;
+                    p[i + j*Nx + m*Nx*Ny].z = m/k+0.1;
+                }
+            }
+        }
+        for(int i=N-1; i>Nx*Ny*Nz-1; --i){
+            p[i].x = (i+0.5)/k;
+            p[i].y = (i+0.5)/k;
+            p[i].z = (i+0.5)/k;
+        }
+
+        for(int i = 0; i<N-1; ++i){
+            for(int j = i+1; j<N; ++j){
+                count_forces(i, j);
+            }
+        }
+    }
 
     void set_random_speed(double mean, double std)
     {
         std::normal_distribution <double> disv(mean, std);
 
-        for(int i=0; i<N; i++)
-        {
+        for(int i=0; i<N; i++){
             p[i].v_x = disv(rng);
             p[i].v_y = disv(rng);
             p[i].v_z = disv(rng);
+        }
+
+        double vc_x, vc_y, vc_z;
+        vc_x = vc_y = vc_z = 0.0;
+        double M = 0;
+        for(int i=0; i<N; i++){
+            M += p[i].m;
+            vc_x += p[i].v_x * p[i].m;
+            vc_y += p[i].v_y * p[i].m;
+            vc_z += p[i].v_z * p[i].m;
+        }
+        vc_x /= M;
+        vc_y /= M;
+        vc_z /= M;
+        for(int i=0; i<N; i++){
+            p[i].v_x -= vc_x;
+            p[i].v_y -= vc_y;
+            p[i].v_z -= vc_z;
         }
     }
 
@@ -147,32 +203,36 @@ public:
 
     void iter(double dt)
     {
-        for(int i = 0; i<N-1; ++i){
-            for(int j = i+1; j<N; ++j){
-                count_forces(i, j);
-            }
-//            change_speed(i, dt/2);
-//            change_position(i, dt);
-        }
         for(int i = 0; i<N; ++i){
             change_speed(i, dt/2);
             change_position(i, dt);
         }
+
         for(int i = 0; i<N-1; ++i){
             for(int j = i+1; j<N; ++j){
                 count_forces(i, j);
             }
-//            change_speed(i, dt/2);
         }
+
         for(int i = 0; i<N; ++i){
             change_speed(i, dt/2);
         }
     }
 
-    void run(double T, double dt)
+    void run(double T, double dt, double tau)
     {
         int I = static_cast<int> (T/dt);
+        int J = static_cast<int> (tau/dt);
         for(int i=0; i<I; ++i){
+            if (i%J == 0){
+                count_energy();
+                count_impulse();
+                std::cout << "E = " << Energy;
+                std::cout << "  Impulse = " << P;
+                std::cout << " T = " << temperature;
+                //std::cout << "  rmin = " << find_min(R, N) << std::endl;
+                save_points("Points_data.txt", true);
+            }
             iter(dt);
         }
     }
@@ -183,7 +243,7 @@ public:
         R[i][j] = r;
         R[j][i] = r;
 
-        F[i][j] = force(r, eps, sigma);
+        F[i][j] = LJP_force(r, eps, sigma);
         F[j][i] = F[i][j];
 
         dx = p[i].x - p[j].x;
@@ -202,17 +262,13 @@ public:
         Fy[i][j] = F[i][j] * dy / r;
         Fz[i][j] = F[i][j] * dz / r;
 
-        Fx[j][i] = Fx[i][j];
-        Fy[j][i] = Fy[i][j];
-        Fz[j][i] = Fz[i][j];
+        Fx[j][i] = -Fx[i][j];
+        Fy[j][i] = -Fy[i][j];
+        Fz[j][i] = -Fz[i][j];
     }
 
     void change_speed(int i, double dt)
     {
-//        p[i].v_x += sim_sum(Fx, i)/p[i].m * dt;
-//        p[i].v_y += sim_sum(Fy, i)/p[i].m * dt;
-//        p[i].v_z += sim_sum(Fz, i)/p[i].m * dt;
-
         p[i].v_x += sum(Fx[i])/p[i].m * dt;
         p[i].v_y += sum(Fy[i])/p[i].m * dt;
         p[i].v_z += sum(Fz[i])/p[i].m * dt;
@@ -234,21 +290,28 @@ public:
         else if (p[i].z < 0) {p[i].z += z_size;}
     }
 
-    double count_energy()
+    void count_energy()
     {
-        double W = 0;
+        Energy = 0.0;
         for(int i = 0; i<N; ++i){
-            W = W + p[i].m * (pow(p[i].v_x, 2) + pow(p[i].v_y, 2) + pow(p[i].v_z, 2)) / 2.0;
-            for(int j = 0; j<N; ++j){
-                if(i!=j){
-                    r = R[i][j];
-                    W = W + 4*eps*(pow((sigma/r),12) - pow((sigma/r),6)) * p[i].m * p[j].m;
-                }
+            Energy += p[i].m * (pow(p[i].v_x, 2) + pow(p[i].v_y, 2) + pow(p[i].v_z, 2)) / 2.0;
+            for(int j = i+1; j<N; ++j){
+                Energy += LJP(R[i][j], eps, sigma);
             }
         }
-        return W;
+        temperature = 2.0/3*Energy/N/k;
     }
 
+    double count_impulse()
+    {
+        Px = Py = Pz = 0.0;
+        for(int i=0; i<N; ++i){
+            Px += p[i].v_x * p[i].m;
+            Py += p[i].v_y * p[i].m;
+            Pz += p[i].v_z * p[i].m;
+        }
+        P = vec_mod(Px, Py, Pz);
+    }
 
     void Maxwell_check()
     {
@@ -284,9 +347,15 @@ public:
         }
     }
 
-    void save_points(std::string path)
+    void save_points(std::string path, bool save)
     {
-        std::ofstream out(path);
+        std::ofstream out;
+        if (save){
+            out.open(path, std::ios::app);
+        }
+        else{
+            out.open(path);
+        }
         if (out.is_open()){
             out << N << '\n';
             out << " \n";
@@ -294,11 +363,18 @@ public:
                 out << i << ' ' << p[i].x << ' ' << p[i].y << ' ' << p[i].z << '\n';
             }
         }
+        out.close();
     }
 
-    void save_speed(std::string path)
+    void save_speed(std::string path, bool save)
     {
-        std::ofstream out(path);
+        std::ofstream out;
+        if (save){
+            out.open(path, std::ios::app);
+        }
+        else{
+            out.open(path);
+        }
         if (out.is_open()){
             out << N << '\n';
             out << " \n";
@@ -306,56 +382,110 @@ public:
                 out << i << ' ' << p[i].v_x << ' ' << p[i].v_y << ' ' << p[i].v_z << '\n';
             }
         }
+        out.close();
     }
 
     void load_points(std::string path)
     {
         std::ifstream out(path);
-
+        int a;
         if (out.is_open()){
              out >> N;
              for(int i=0; i<N; ++i){
-                out >> p[i].x >> p[i].y >> p[i].z;
+                out >> a >> p[i].x >> p[i].y >> p[i].z;
              }
         }
     }
 
-    void load_velocity(std::string path)
+    void load_speed(std::string path)
     {
         std::ifstream out(path);
-
+        int a;
         if (out.is_open()){
              out >> N;
              for(int i=0; i<N; ++i){
-                out >> p[i].v_x >> p[i].v_y >> p[i].v_z;
+                out >> a >> p[i].v_x >> p[i].v_y >> p[i].v_z;
              }
         }
     }
 };
 
-int main()
+
+template<typename type>
+void process(std::string path)
 {
-    int n = 45;
-    double T = 1.0;
-    double dt = 1/pow(2, 10);
-    double tau = 0.01;
-    Space s(n, 35.0, 35.0, 35.0);
-    s.set_random_points();
-    s.set_random_speed(0, 1);
+    unsigned int N;
+    type T, dt, tau, x_size, y_size, z_size, eps, sigma, v_mean, v_std;
+    std::ifstream out(path);
+    std::string str1, str2;
+    if (out.is_open()){
+        getline(out, str2);
+        getline(out, str1);
+        N = stoi(str1);
 
-    //std::cout << s.count_energy() << std::endl;
+        getline(out, str2);
+        getline(out, str1);
+        x_size = stod(str1);
 
-    int I = static_cast<int> (T/dt);
-    int J = static_cast<int> (tau/dt);
-    for(int i=0; i<I; ++i){
-        if (i%J == 0){
-            std::cout << "E = " << s.count_energy();
-            std::cout << "  rmin = " << find_min(s.R, n) << std::endl;
-        }
-        s.iter(dt);
+        getline(out, str2);
+        getline(out, str1);
+        y_size = stod(str1);
+
+        getline(out, str2);
+        getline(out, str1);
+        z_size = stod(str1);
+
+        getline(out, str2);
+        getline(out, str1);
+        T = stod(str1);
+
+        getline(out, str2);
+        getline(out, str1);
+        dt = stod(str1);
+
+        getline(out, str2);
+        getline(out, str1);
+        tau = stod(str1);
+
+        getline(out, str2);
+        getline(out, str1);
+        eps = stod(str1);
+
+        getline(out, str2);
+        getline(out, str1);
+        sigma = stod(str1);
+
+        getline(out, str2);
+        getline(out, str1);
+        v_mean = stod(str1);
+
+        getline(out, str2);
+        getline(out, str1);
+        v_std = stod(str1);
     }
 
-    //s.save_points("Points_data.txt");
+    std::cout << N << " particles" << '\n';
+
+    Space s(N, x_size, y_size, z_size, eps, sigma);
+    s.set_random_points();
+    //s.set_grid_points();
+    s.set_random_speed(v_mean, v_std);
+
+    s.save_points("Points_data.txt", false);
+
+    //s.load_points("Points_data.txt");
+    //s.load_speed("Speed_data.txt");
+
+    s.run(T, dt, tau);
+
+    //s.save_points("Points_data.txt", true);
+}
+
+
+int main()
+{
+    std::string path = "Params.txt";
+    process<double>(path);
 
     return 0;
 }
